cef59e0a0f5f8fea397d13a73baf7e9a154f6baa
Qinghao Du <qdu3@sheffield.ac.uk>
2022-03-30 19:25:04 +0100

    solved javascript loading problem, and added login_btn to login page

---
+++ app/javascript/packs/login_btn.js
@@ -0,0 +1,28 @@
+window.onload = function(){ // the function that check if user input is blank
+        var username = document.getElementById('username');
+        var password = document.getElementById('password')
+        var btn = document.getElementById('btn');
+
+        btn.onclick = function(){
+            var user_value = username.value.replace(/\s+/g,"");
+            var password_value = password.value.replace(/\s+/g,"");
+            // check if there is input
+            if(user_value == '' || user_value == ' '){
+                username.value = "" // reset the input filed
+                username.classList.add('animate__animated', 'animate__wobble'); //add animation
+                username.addEventListener('animationend', () => { // reset animation
+                    username.classList.remove('animate__animated', 'animate__wobble')
+                });
+            }
+            else if(password_value == '' || password_value == ' '){
+                password.value = "" // reset the input filed
+                password.classList.add('animate__animated', 'animate__wobble'); //add animation
+                password.addEventListener('animationend', () => { // reset animation
+                    password.classList.remove('animate__animated', 'animate__wobble')
+                });
+            }
+
+
+
+        }
+    }
