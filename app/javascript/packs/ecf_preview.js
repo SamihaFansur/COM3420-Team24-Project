bcacd9bf3f19f257aa06eb1bc2e8ccb39808e836
Qinghao Du <qdu3@sheffield.ac.uk>
2022-03-31 20:46:19 +0100

    added css to ecf preview page (after login)

---
+++ app/javascript/packs/ecf_preview.js
@@ -0,0 +1,13 @@
+window.onload = function(){ // the function that check if user input is blank
+    //check if there is ecfs
+    if(document.getElementById('num1').innerText != 0) {
+        document.getElementById('num1').classList.add('num_colour');
+        document.getElementById('col1').classList.add('animate__animated', 'animate__bounce');
+    }
+    if(document.getElementById('num2').innerText != 0){
+        document.getElementById('num2').classList.add('num_colour');
+        document.getElementById('col2').classList.add('animate__animated', 'animate__bounce');
+    }
+
+
+}
