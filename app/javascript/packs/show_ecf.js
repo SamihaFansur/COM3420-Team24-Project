86a8079801aab2b170cf01c6fa0db25aa6c74198
Qinghao Du <qdu3@sheffield.ac.uk>
2022-04-04 22:44:06 +0100

    added css to show ecf page, but need some method update for read data from ecfs table

---
+++ app/javascript/packs/show_ecf.js
@@ -0,0 +1,16 @@
+window.onload = function(){ // the function that check if user input is blank
+    //check if there is ecfs
+    var text_area = document.getElementById("exampleFormControlTextarea1");
+    text_area.classList.add("select_without_background"); // overwrite default blue background for better readability
+    text_area.onfocus = function (){
+        text_area.setAttribute('style', 'height: 200px !important') // for better input
+        this.style.borderStyle = "outset"
+    }
+    text_area.onblur= function (){
+        this.style.borderStyle = "inset"
+        text_area.setAttribute('style', 'height: 100px !important') // for better input
+    }
+
+
+
+}
