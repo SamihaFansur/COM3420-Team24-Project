89f60d95a6dbd297a3bfbc17810d6b77e15dd4c7
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2022-04-04 21:59:19 +0100

    Added tests for creating new ecf

---
+++ spec/features/new_ecf_spec.rb
@@ -0,0 +1,30 @@
+require_relative "../spec_helper"
+require 'rails_helper'
+
+#Test Login
+
+describe "ecf" do
+    #log in as a user
+    it "logs is and visits ecf page " do
+        visit"/users/sign_in"
+        login_as(FactoryBot.create(:student))
+        visit"/ecfs"
+        expect(page).to have_content "Listing"
+    end
+end
+
+describe "ecf" do
+    #log in as a user
+    it "fills out and submits ecf " do
+        visit"/users/sign_in"
+        login_as(FactoryBot.create(:student))
+        visit"/ecfs"
+        click_link "Create New ECF"
+        fill_in "Details", with: "Example User2"
+        fill_in "Unit code", with: "COM2008"
+        fill_in "Assessment type", with: "Exam"
+        select "DEX - Deadline Extension", from: "Requested action ", visible: false
+        click_button "Create Ecf"
+        expect(page).to have_content "Example User2"
+    end
+end
