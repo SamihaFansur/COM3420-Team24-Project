0a0424f388baecaa8ab6e485738902be596631aa
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2022-04-09 10:18:46 +0200

    Updated user factory and added atendee notifaction test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,23 +2,29 @@
 #
 # Table name: users
 #
-#  id                 :bigint           not null, primary key
-#  current_sign_in_at :datetime
-#  current_sign_in_ip :string
-#  dn                 :string
-#  email              :string           default(""), not null
-#  givenname          :string
-#  last_sign_in_at    :datetime
-#  last_sign_in_ip    :string
-#  mail               :string
-#  ou                 :string
-#  role               :integer          default("guest")
-#  sign_in_count      :integer          default(0), not null
-#  sn                 :string
-#  uid                :string
-#  username           :string
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
+#  id                  :bigint           not null, primary key
+#  current_sign_in_at  :datetime
+#  current_sign_in_ip  :string
+#  dn                  :string
+#  dob                 :date
+#  email               :string           default(""), not null
+#  givenname           :string
+#  is_student_visa     :boolean
+#  last_sign_in_at     :datetime
+#  last_sign_in_ip     :string
+#  level_of_study      :string
+#  mail                :string
+#  ou                  :string
+#  programme_of_study  :string
+#  registration_number :integer
+#  role                :integer          default("guest")
+#  sign_in_count       :integer          default(0), not null
+#  signature           :string
+#  sn                  :string
+#  uid                 :string
+#  username            :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
 #
 # Indexes
 #
@@ -35,11 +41,12 @@ FactoryBot.define do
 
     factory :student do
       dn {"uid=aca20sf,ou=Undergraduates,ou=Students,ou=Users,dc=sheffield,dc=ac,dc=uk"}
+      role {1}
 
     end
     factory :staff do
       dn {"uid=aca20sf,ou=Undergraduates,ou=Staff,ou=Users,dc=sheffield,dc=ac,dc=uk"}
-
+      role {4}
     end
   end
 end
--- spec/features/meetings_spec.rb
+++ spec/features/meetings_spec.rb
@@ -46,5 +46,29 @@ describe "meeting" do
         fill_in "Attendees", with: "new test"
         click_button "Update Meeting"
         expect(page).to have_content "new test"
+
+    end
+end
+
+describe "meeting" do
+    #log in as a user
+    it "fills out and submits meetings then updates it" do
+        visit"/users/sign_in"
+        login_as(FactoryBot.create(:student))
+        visit"/meetings"
+        click_link "New Meeting"
+        fill_in "Title", with: "1"
+        fill_in "Attendees", with: "test"
+        click_button "Create Meeting"
+        expect(page).to have_content "Attendees: test"
+        visit"/meetings"
+        expect(page).to have_content "test"
+        click_link "Edit"
+        fill_in "Attendees", with: "new test"
+        click_button "Update Meeting"
+        expect(page).to have_content "new test"
+        visit"/meetings"
+        click_link "Email Attendees", match: :first, visible: false
+        expect(page).to have_content "Attendees notified"
     end
 end
