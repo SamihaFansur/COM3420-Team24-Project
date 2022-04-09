5babc1facb9d151dd2442149d9201b458589e5f4
Ezra Bell <ebell3@sheffield.ac.uk>
2022-04-09 22:55:19 +0100

    CSV uploading of M:M users, modules. Avoids duplicates of both existing and new from csv users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,29 +38,75 @@ class UsersController < ApplicationController
   end
 
   def import
-    columns = ["email"]
     path = params[:user][:file].tempfile.path
+    # new users to commit to the database
     users = []
+    # new user_modules to commit to the database
     user_modules = []
 
-    lists_hash = UserModule.pluck(:module_code, :id).to_h
+    # hash of existing users and their ids {"email1@test.com => 23"}
+    users_hash = User.pluck(:email, :id).to_h
+    # hash of existing users and their module codes {"email1@test.com" => ["COM1004", "COM1010"]}
+    user_modules_array = UserModule.select(:user_id, 'array_agg(module_code)').group(:user_id).pluck(:user_id, 'array_agg(module_code)')
+    user_modules_hash = Hash[*user_modules_array.flatten(1)]
+    # hash to track new users from csv {"email3@test.com => User#{...}"}
+    new_users_hash = Hash.new
+    # hash to track new user modules from csv {"email3" => ["COM2003", "COM3004"]}
+    new_user_modules_hash = Hash.new {|x,y| x[y] = []}
+
+    #basic. does repeat existing users
     CSV.foreach(path, headers: true) do |row|
-      unless row.to_h["email"].nil?
-        user = User.new(email: row.to_h["email"], role: 2)
-        user.get_info_from_ldap
-        users << user unless user.username.nil?
-        if lists_hash.has_key?(row.to_h["module_code"])
-          user_module = UserModule.where(module_code: row.to_h["module_code"])
-          user_module.user_id = user.id
-          user_modules << user_module
+      email = row.to_h["email"]
+      module_code = row.to_h["module_code"]
+
+      unless email.nil?
+        # if the user already exists in the database
+        if users_hash.key?(email)
+          user = User.find_by(email: email)
+          user.get_info_from_ldap
+          
+          # only tries 'include' if user has any modules
+          if user_modules_hash.key?(user.id.to_s)
+            unless user_modules_hash[user.id.to_s].include? module_code or new_user_modules_hash[email].include? module_code
+              new_user_modules_hash[email] << module_code
+              user_module = UserModule.new(module_code: module_code, user: user)
+              user_modules << user_module
+            end
+          else
+            unless new_user_modules_hash[email].include? module_code
+              new_user_modules_hash[email] << module_code
+              user_module = UserModule.new(module_code: module_code, user: user)
+              user_modules << user_module
+            end
+          end
         else
-          user_module = UserModule.new(module_code: row.to_h["module_code"], user_id: user.id)
-          user_modules << user_module
+          # only add queue this user for creation if not added by a previous row
+          unless new_users_hash.key?(email)
+            user = User.new(email: email, role: 2)
+            user.get_info_from_ldap
+            new_users_hash[email] = user
+            users << user
+          else
+            user = new_users_hash[email]
+          end
+
+          # only add module if not added by a previous row
+          if new_user_modules_hash.key?(email)
+            unless new_user_modules_hash[email].include? module_code
+              new_user_modules_hash[email] << module_code
+              user_module = UserModule.new(module_code: module_code, user: user)
+              user_modules << user_module
+            end
+          else
+            new_user_modules_hash[email] << module_code
+            user_module = UserModule.new(module_code: module_code, user: user)
+            user_modules << user_module
+          end
         end
       end
     end
-    puts user_modules
-    # User.import users, batch_size: 2000
+
+    User.import users, batch_size: 2000
     UserModule.import user_modules, batch_size: 1000
 
     redirect_to users_path, notice: "Users imported successfully."
