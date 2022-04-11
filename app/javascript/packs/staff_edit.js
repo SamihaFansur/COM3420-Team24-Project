3847d1c01af010481a7296ed1a24b0447fda0891
Qinghao Du <qdu3@sheffield.ac.uk>
2022-04-11 22:54:49 +0100

    improved css and js of staff edit page, need a method to prevent admin update evidence while no file uploaded.

---
+++ app/javascript/packs/staff_edit.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+    var text_area = document.getElementById("exampleFormControlTextarea1");
+    text_area.onfocus = function (){
+        text_area.setAttribute('style', 'height: 200px !important') // enlarge text area
+        this.style.borderStyle = "outset"
+    }
+    text_area.onblur= function (){
+        this.style.borderStyle = "inset"
+        text_area.setAttribute('style', 'height: 100px !important') // enlarge text area
+    }
+
+    const upload_file = document.getElementById("ecf_upload_medical_evidence"); // check upload file type validation
+    upload_file.onchange = function(){
+        var file_number = this.files.length;
+        for (var i=0; i < file_number; i++ ){
+            if(this.files[i].type != "application/pdf"){
+                alert("please check the file type before uploading");
+                this.value = "";
+                return;
+            }
+        }
+    }
+
+    document.getElementById("update_evi_btn").onclick = function(){
+        var file_number = document.getElementById("ecf_upload_medical_evidence").files.length;
+        if(file_number == 0){
+            alert("please upload at least one file");
+            return;
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
