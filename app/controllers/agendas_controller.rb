50ddc408879fb4420c79826f6ca1acee9ed9e473
Ezra Bell <ebell3@sheffield.ac.uk>
2022-04-12 22:48:20 +0100

    Can now add ECFS to a meeting's agenda through 'Edit ECF'.

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -2,13 +2,17 @@ class AgendasController < ApplicationController
   # load_and_authorize_resource
 
   def create
-    agenda = Agenda.new(meeting_id: params[:meeting_id], ecf_id: params[:ecf_id])
+    agenda = Agenda.new(agenda_params)
+    print("\n PRINTING PARAMS: \n")
+    print(agenda_params)
+    print(params[:meeting_id])
+    print(params[:ecf_id])
     if agenda.save
       flash[:notice] = "ECF #{params[:ecf_id]} was successfully added to the meeting."
       redirect_back(fallback_location: ecfs_path)
     else
       # add extra validation here to clarify whether failed on uniqueness or just db error.
-      if Agenda.where(meeting_id: params[:meeting_id], ecf_id: params[:ecf_id]).exists?
+      if Agenda.where(agenda_params).exists?
         flash[:alert] = "This ECF has already been added to the meeting!"
       else
         flash[:alert] = "Failed to add ECF to the meeting."
@@ -17,4 +21,10 @@ class AgendasController < ApplicationController
     end
   end
 
+  private
+    def agenda_params
+      params
+        .require(:agenda)
+        .permit(:ecf_id, :meeting_id)
+    end
 end
