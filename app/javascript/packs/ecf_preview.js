8d0f48fd7d3f599f2d15e80b5b373f3e13c50bbe
Qinghao Du <qdu3@sheffield.ac.uk>
2022-04-14 00:28:06 +0100

    fixed a bug in ecf_preview.js, and updated css for new_form.html.haml

--- app/javascript/packs/ecf_preview.js
+++ app/javascript/packs/ecf_preview.js
@@ -4,10 +4,6 @@ window.onload = function(){
         document.getElementById('num1').classList.add('num_colour');
         document.getElementById('col1').classList.add('animate__animated', 'animate__bounce');
     }
-    if(document.getElementById('num2').innerText != 0){
-        document.getElementById('num2').classList.add('num_colour');
-        document.getElementById('col2').classList.add('animate__animated', 'animate__bounce');
-    }
 
 
 }
