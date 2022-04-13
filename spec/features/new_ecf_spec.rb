a5a9796701864ce4f49b8ac8956a70a1e086d7a4
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2022-04-13 21:42:13 +0200

    Added test for showing students 'my ecfs' and added new test user

--- spec/features/new_ecf_spec.rb
+++ spec/features/new_ecf_spec.rb
@@ -16,6 +16,7 @@ end
 describe "ecf" do
     #log in as a user
     it "fills out and submits ecf " do
+        #login in as student 1 and make an ecf
         visit"/users/sign_in"
         login_as(FactoryBot.create(:student))
         visit"/ecfs"
@@ -25,6 +26,28 @@ describe "ecf" do
         fill_in "Assessment type", with: "Exam"
         select "DEX - Deadline Extension", from: "Requested action ", visible: false
         click_button "Create Ecf"
+        #Check that ecf is listed fro student 1
         expect(page).to have_content "arb20eg"
+        click_link "mdo"
+        click_link "Sign out"
+        expect(page).to have_content "Overview"
+        #Logged out and login as student 2
+        visit"/users/sign_in"
+        login_as(FactoryBot.create(:student2))
+        visit"/ecfs"
+        click_link "MY ECFS"
+        #Check that student 1 user ecf doesnt show up for student 2
+        expect(page).not_to have_content "arb20eg"
+        #Create ecf for student 2
+        visit"/ecfs"
+        click_link "Create New ECF"
+        fill_in "Details", with: "Example User2"
+        fill_in "Unit code", with: "COM2008"
+        fill_in "Assessment type", with: "Exam"
+        select "DEX - Deadline Extension", from: "Requested action ", visible: false
+        click_button "Create Ecf"
+        #Check student 2 sees their ecf
+        expect(page).to have_content "aca20sf"
+
     end
 end
