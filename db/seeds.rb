52ec1dac81a809c09ca5165db813553c194259e6
Ezra Bell <ebell3@sheffield.ac.uk>
2022-05-01 00:05:22 +0100

    Nil outcome fixes, info on new outcome.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+outcomes = Outcome.create([
+  {name: 'Approved', short_name: 'Y', description: 'Approved'},
+  {name: 'Refused', short_name: 'N', description: 'Justification not accepted'},
+  {name: 'Need more evidence', short_name: 'E', description: 'Require more evidence'},
+  {name: 'Ignore until next meeting', short_name: 'I', description: 'Ignore until next meeting'},
+  {name: 'Consider at final board', short_name: 'C', description: 'Insufficient reason for action now but will reconsider at end of year'}
+])
