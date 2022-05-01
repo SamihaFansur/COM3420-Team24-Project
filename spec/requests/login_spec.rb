dd2fa1da64e94b662926e27d0ad2e31a2b7b3be7
Ariful Haque <ahaque3@sheffield.ac.uk>
2022-05-01 20:12:37 +0100

    Fix login spec

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Logins", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/login/index"
-      expect(response).to have_http_status(:success)
+      visit "/users/sign_in"
+      expect(page).to have_content "ECF Login"
+      expect(page).to have_current_path(new_user_session_path)
     end
   end
 
