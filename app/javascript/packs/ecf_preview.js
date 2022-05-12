efbb2e920cefaf95ffe30475db0c7851400cbe0c
Qinghao Du <qdu3@sheffield.ac.uk>
2022-05-12 14:01:23 +0100

    removed bounce and other animations.

--- app/javascript/packs/ecf_preview.js
+++ app/javascript/packs/ecf_preview.js
@@ -3,6 +3,5 @@ window.onload = function(){
   //check if there is ecfs, add formatting classes.
   if (document.getElementById('num1').innerText != 0) {
     document.getElementById('num1').classList.add('num_colour');
-    document.getElementById('col1').classList.add('animate__animated', 'animate__bounce');
   }
 }
