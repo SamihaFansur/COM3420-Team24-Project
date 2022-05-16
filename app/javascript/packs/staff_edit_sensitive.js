770540cd7d6bbe412c9dfa05beb11f891b092d73
Qinghao Du <qdu3@sheffield.ac.uk>
2022-05-16 15:40:15 +0100

    deleted unused js file staff_edit.js, added validation for upload email conversation to prevent a 403 page.

--- app/javascript/packs/staff_edit_sensitive.js
+++ app/javascript/packs/staff_edit_sensitive.js
@@ -38,5 +38,28 @@ window.onload=function(){
             alert("please select a file");
         }
     }
+    // checks the file type of each uploaded document from file explorer; ensure it is a pdf.
+    const upload_conv = document.getElementById("ecf_upload_conversations");
+    upload_conv.onchange = function(e) {
+        var file_number = this.files.length;
+
+        for (var i=0; i < file_number; i++ ) {
+            if (this.files[i].type != "application/pdf") {
+                alert("please check the file type is .pdf before uploading");
+                this.value = "";
+                handleClickBtn(e); // reset the file explorer
+                return;
+            }
+        }
+    }
+    // check upload file type validation for medical evidence on 'Upload evidence' button select.
+    const conv_btn = document.getElementById("update_conv_btn");
+    conv_btn.onclick = function(e){
+        var file_number = document.getElementById("ecf_upload_conversations").files.length;
+        if (file_number == 0) {
+            handleClickBtn(e); //prevent error page, handle case of no evidence
+            alert("please select a file");
+        }
+    }
 
 }
